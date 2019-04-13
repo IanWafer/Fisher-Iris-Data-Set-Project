7bc9b4f3a0e432166be854d7276e9f458665bc41
IanWafer <g00376322@gmit.ie>
2019-04-13 15:00:37 +0100

    Updated code

--- iris.py
+++ iris.py
@@ -8,10 +8,20 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-iris = pd.read_csv('iris.csv')
+iris = pd.read_csv("input\iris.csv")
 
-plt.scatter(iris['sepal_length'], iris['sepal_width'], iris['petal_width'], iris['petal_length'])
+sns.set(style="ticks")
+g = sns.relplot(x="sepal_length", y="sepal_width", hue="species", palette=["b", "r", "g"], col="species", data=iris)
+
+
+plt.subplots_adjust(top=0.85)
+g.fig.suptitle("Test")
+
+h = sns.relplot(x="sepal_length", y="sepal_width", hue="species", palette=["b", "r", "g"], data=iris)
+
+# The below adds a title to the combined flower output chart
+h.fig.suptitle("Overlapping Species Sizes")
 
 plt.show()
-
-# https://stackoverflow.com/questions/42777946/plotting-from-dataset-in-python
+# https://seaborn.pydata.org/generated/seaborn.FacetGrid.html
+# https://stackoverflow.com/questions/54209895/seaborn-relplot-how-to-control-the-location-of-the-legend-and-add-title
