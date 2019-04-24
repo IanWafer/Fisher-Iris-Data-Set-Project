8fffc72eccc85843e80036c96ed315595c3be8c0
IanWafer <g00376322@gmit.ie>
2019-04-24 17:45:22 +0100

    Code and readme updated

--- iris.py
+++ iris.py
@@ -72,39 +72,8 @@ plt.ylabel('Quantity')
 plt.savefig('images\Outputs\Hist-Petal_Width.png')
 plt.show()
 
-# Setup style of graphs to be created. Creat individual graphs species type with breakdown of colour changed to pallette parameters
-sns.set(style='darkgrid')
-g = sns.relplot(x='sepal_length', y='sepal_width', hue='species', palette=['b', 'r', 'g'], col='species', data=iris)
-
-# Lower subplot heading to creat room for overall title and change axis labels
-plt.subplots_adjust(top=0.85)
-g.fig.suptitle('Individual Flower Sepal Sizes')
-g.set_axis_labels('Sepal Length (cm)', 'Sepal Width (cm)')
-
-# Setup second graph with overlapping species on one graph seperated by colour.
-h = sns.relplot(x='sepal_length', y='sepal_width', hue='species', palette=['b', 'r', 'g'], data=iris)
-
-# The below adds a title to the combined flower output chart
-h.fig.suptitle('Overlapping Species Sepal Sizes')
-h.set_axis_labels('Sepal Length (cm)', 'Sepal Width (cm)')
-
-# Duplicate above for petal dimensions
-
-sns.set(style='darkgrid')
-i = sns.relplot(x='petal_length', y='petal_width', hue='species', palette=['b', 'r', 'g'], col='species', data=iris)
-
-plt.subplots_adjust(top=0.85)
-i.fig.suptitle('Individual Flower Petal Sizes')
-i.set_axis_labels('Petal Length (cm)', 'Petal Width (cm)')
-
-j = sns.relplot(x='petal_length', y='petal_width', hue='species', palette=['b', 'r', 'g'], data=iris)
-
-j.fig.suptitle('Overlapping Species Petal Sizes')
-j.set_axis_labels('Petal Length (cm)', 'Petal Width (cm)')
-
-plt.show()
-
 # Setup for violinplot display with datapoints highlighted in white using swarmplot and print graph 
+sns.set(style='darkgrid')
 sns.violinplot(x='species', y='sepal_length', palette=['b', 'r', 'g'], data=iris, inner=None)
 sns.swarmplot(x='species', y='sepal_length', data=iris, color='white', edgecolor='gray', size=4)
 plt.savefig('images\Outputs\Violin-Sepal_Length.png')
@@ -125,7 +94,7 @@ sns.swarmplot(x='species', y='petal_width', data=iris, color='white', edgecolor=
 plt.savefig('images\Outputs\Violin-Petal_Width.png')
 plt.show()
 
-sns.pairplot(hue='species', markers='x', data=iris, palette=['b', 'r', 'g'])
+sns.pairplot(hue='species', markers='o', data=iris, palette=['b', 'r', 'g'])
 plt.savefig('images\Outputs\pairplot.png')
 plt.show()
 
